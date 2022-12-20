0551d537b82c6cd94efa8d8a884c12e4e64f9351
AbdulTheActivePiecer <abdulyki@activepieces.com>
2022-12-20 22:02:26 +0300

    export connector + update versions

--- src/worker/packages/components/src/apps/index.ts
+++ src/worker/packages/components/src/apps/index.ts
@@ -2,9 +2,11 @@ import {slack} from './slack';
 import type {Component} from '../framework/component';
 import { gmail } from './gmail';
 import { facebook } from './facebook';
+import { googleSheets } from './google-sheets';
 
 export const apps: Component[] = [
 	slack,
 	gmail,
-	facebook
+	facebook,
+	googleSheets
 ];
